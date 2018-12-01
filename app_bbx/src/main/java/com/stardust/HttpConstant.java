bbece1eeba7ef8b68233e05d37930954935f1053
Chenll <woshicll88@126.com>
2018-12-01 13:15:04 +0800

    12

--- app_bbx/src/main/java/com/stardust/HttpConstant.java
+++ app_bbx/src/main/java/com/stardust/HttpConstant.java
@@ -1,7 +1,7 @@
 package com.stardust;
 
 public class HttpConstant {
-    private static final String URL_BASE = "http://bbx.youjiuoa.com/";
+    private static final String URL_BASE = "http://bbxapi.youjiuoa.com/";
     public static final String URL_GETTASK = URL_BASE + "UserKey.ashx";
     public static final String URL_UPLOAD = URL_BASE + "UpdatePoints.ashx";
     public static final String URL_UPDATA = URL_BASE + "ApkVersion.ashx";
